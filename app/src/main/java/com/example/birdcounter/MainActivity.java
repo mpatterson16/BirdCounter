c9f4177db35ccbabfad7a9c65f09f8158cf1c252
Patterson <maeganpatterson16@augustana.edu>
2020-02-13 12:11:29 -0600

    Bird progress

--- app/src/main/java/com/example/birdcounter/MainActivity.java
+++ app/src/main/java/com/example/birdcounter/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     Button eagleMinusButton;
     Button eaglePlusButton;
-    EditText eagleNumber;
+    TextView eagleNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         eagleMinusButton=(Button)findViewById(R.id.eagleMinusButton);
         eaglePlusButton=(Button)findViewById(R.id.eaglePlusButton);
-        eagleNumber=(EditText)findViewById(R.id.eagleNumber);
+        eagleNumber=(TextView)findViewById(R.id.eagleNumber);
 
         eaglePlusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                eagleNumber.setText((Integer.parseInt(eagleNumber.getText().toString()) + 1) + "");
+            }
+        });
+
+        eagleMinusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Integer.parseInt(eagleNumber.getText().toString()) > 0) {
+                    eagleNumber.setText((Integer.parseInt(eagleNumber.getText().toString()) - 1) + "");
+                }
+
 
             }
         });
